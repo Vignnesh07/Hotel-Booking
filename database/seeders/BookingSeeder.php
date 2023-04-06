41690b0c07273c62377a83293151134e2328b8be
Vignnesh <ravin.vignnesh@gmail.com>
2023-04-06 21:01:23 +0800

    Fixed db seeding

--- database/seeders/BookingSeeder.php
+++ database/seeders/BookingSeeder.php
@@ -2,11 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\Booking;
 use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-//use Illuminate\Support\Facades\Hash;
 
 class BookingSeeder extends Seeder {
     /**
@@ -14,29 +12,24 @@ class BookingSeeder extends Seeder {
      */
     public function run(): void
     {
-
         DB::table('bookings') -> insert([
-            Booking::create([
-                // 'id' => '001',
-                'fname' => 'Joanne',
-                'lname' => 'Lim',
-                'roomtype' => 'Single',
-                'roomnumber' => 'SI001',
-                'email' => 'jlzy21@icloud.com',
-                'idcard' => '010121137899',
-                'phone' => '+60166638568',
-                'residentialaddress' => 'Lot 123, Jalan Tasik',
-                'city' => 'Miri',
-                'zipcode' => '98000',
-                'amount' => 800,
-                'paidamount' => 400,
-                'deposit' => 200,
-                'checkindate' => '2023-03-28',
-                'checkoutdate' => '2023-04-03',
-                'checkedin' => true,
-                'checkedout' => false,
-
-            ]),
+            'fname' => 'Joanne',
+            'lname' => 'Lim',
+            'roomtype' => 'Single',
+            'roomnumber' => 'SI001',
+            'email' => 'jlzy21@icloud.com',
+            'idcard' => '010121137899',
+            'phone' => '+60166638568',
+            'residentialaddress' => 'Lot 123, Jalan Tasik',
+            'city' => 'Miri',
+            'zipcode' => '98000',
+            'amount' => 800,
+            'paidamount' => 400,
+            'deposit' => 200,
+            'checkindate' => '2023-03-28',
+            'checkoutdate' => '2023-04-03',
+            'checkedin' => true,
+            'checkedout' => false,
         ]);
     }
 }
